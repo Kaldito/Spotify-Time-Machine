88ae15235996a4cc9af382e5d4f963de61c41f1f
Kaldo <dantekaled100@gmail.com>
2022-10-04 22:16:15 -0500

    Improvement of the user experience during the script.

--- main.py
+++ main.py
@@ -53,6 +53,17 @@ sp = spotipy.Spotify(auth_manager=SpotifyOAuth(client_id=CLIENT_ID,
 
 user_id = sp.current_user()["id"]
 
+print(""" 
+  _____ ____   ___   ______  ____  _____  __ __      ______  ____  ___ ___    ___      ___ ___   ____    __  __ __  ____  ____     ___ 
+ / ___/|    \ /   \ |      ||    ||     ||  |  |    |      ||    ||   |   |  /  _]    |   |   | /    |  /  ]|  |  ||    ||    \   /  _]
+(   \_ |  o  )     ||      | |  | |   __||  |  |    |      | |  | | _   _ | /  [_     | _   _ ||  o  | /  / |  |  | |  | |  _  | /  [_ 
+ \__  ||   _/|  O  ||_|  |_| |  | |  |_  |  ~  |    |_|  |_| |  | |  \_/  ||    _]    |  \_/  ||     |/  /  |  _  | |  | |  |  ||    _]
+ /  \ ||  |  |     |  |  |   |  | |   _] |___, |      |  |   |  | |   |   ||   [_     |   |   ||  _  /   \_ |  |  | |  | |  |  ||   [_ 
+ \    ||  |  |     |  |  |   |  | |  |   |     |      |  |   |  | |   |   ||     |    |   |   ||  |  \     ||  |  | |  | |  |  ||     |
+  \___||__|   \___/   |__|  |____||__|   |____/       |__|  |____||___|___||_____|    |___|___||__|__|\____||__|__||____||__|__||_____|
+                                                                                                                                       
+""")
+
 song_func = best_songs()
 
 songs = song_func["songs"]
@@ -60,16 +71,30 @@ year = song_func["year"]
 date = song_func["date"]
 url_list = []
 
+print("\nTime travel has been successfully completed. Bringing to the present the songs... ")
+print("The songs we could not bring back to the present were: \n")
+
+track = 0
+
 for song in songs:
     url = sp.search(q=f"track:{song} year:{year}", limit=1, type="track", market="MX")
     try:
         url_list.append(url["tracks"]["items"][0]["external_urls"]["spotify"])
+    except TypeError:
+        print(f"{song} not founded in the past.")
     except IndexError:
-        print(f"{song} not founded in spotify.")
+        print(f"{song} not founded in the past.")
+    else:
+        track += 1
+
+if track == 100:
+    print("None. We could bring back to the present all the songs. :)")
 
 playlist = sp.user_playlist_create(user=user_id, name=f"{date} Billboard 100")
 playlist_id = playlist["id"]
 
 sp.playlist_add_items(playlist_id=playlist_id, items=url_list)
+
+print("\nThanks for your visit!")
 
 
